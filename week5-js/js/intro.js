119eeda104966561ad09264aa6585b1d0a705921
Steve Solt <stevesolt@gmail.com>
2016-02-11 13:01:12 -0700

    moved notes & js pages to week5-js

---
+++ week5-js/js/intro.js
@@ -0,0 +1,39 @@
+// alert("Hello");
+var message = "Hello";
+
+var x = 27;
+var temps = [14, 32, 36, 40];
+var person = {
+   first: "Nate", last: "White", address: {
+      city: "Fort Collins", zip: 80521
+   }
+};
+
+document.getElementById('user')
+   <input id="user" type="text">
+user = document.getElementById('user')
+console.log(temps[2]);
+
+console.log(person.address.city  + " " +  person.address.zip);
+
+if (person.address.zip === 80521) {
+   console.log("Hello " + person.first );
+} else {
+   console.log("What are you doing here " + person.first);
+}
+
+document.body.offsetHeight;
+
+
+switch (age) {
+   case 21:
+            console.log("I'm 21");
+            break;
+   case 30:
+            console.log("I'm 30");
+            break
+   default:
+            console.log("I'm " + age);
+            break;
+}
+// can use switch as a parser
--- week5-js/notes20160210.js
+++ week5-js/notes20160210.js
@@ -1,15 +1,15 @@
-
+2016 02 10
 http://www.nwhite.net/
 Nate explained many of his example pages with html, css and tied together with js.
 He will post the link...
 
-
 // run debugger in console
 function add(x, y) { debugger; return x+y; }
-undefined
+
 add(3, 4);
 7
 
+Discussion about arguments vs. parameters in functions. David has the definition for these.
 
 jquery.css()
 jquery.animate()
