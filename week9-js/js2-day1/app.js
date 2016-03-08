377bb0fe6975019ffcb785f56fc343155bff530c
Steve Solt <stevesolt@gmail.com>
2016-03-07 20:57:03 -0700

    mid class commit, setting up porfolio dirs

---
+++ week9-js/js2-day1/app.js
@@ -0,0 +1,28 @@
+console.log('Hello World');
+
+function sayHello(word) {
+	console.log("Hello " + word);
+};
+
+// method 1
+var o = {};
+o.name = "David";
+o.address = "123 Main Street";
+o.age = 19;
+o.age = += 5;
+
+//
+// method 2
+var o2 = {
+	name: "David",
+	address: "123 Main Street",
+	age: 19 +5
+};
+
+o2.age +=5;
+
+o2.name = "Mr. " + o2.name + " Jones";
+
+sayHello("Steve");
+//
+//
