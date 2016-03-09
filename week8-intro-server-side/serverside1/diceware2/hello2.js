0b33ed8ca8f4a88ce1abcd2b2937c8d3b2fcba71
Steve Solt <stevesolt@gmail.com>
2016-03-08 23:09:55 -0700

    end of class commit

---
+++ week8-intro-server-side/serverside1/diceware2/hello2.js
@@ -0,0 +1,6 @@
+var redis = require("redis"); // 1
+//var client = redis.createClient(6379, '127.0.0.1', {no_ready_check: true}); // 2
+var client = redis.createClient(6379, '127.0.0.1'); // 2
+var pass = client.get(11111, redis.print); // 3
+client.quit(); // 4
+//console.log(pass);
